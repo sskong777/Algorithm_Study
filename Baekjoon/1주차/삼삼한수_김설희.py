8b87986f54a817880b6343774cdf154159c7fd70
Seolhi <seolhi1212@gmail.com>
2023-03-17 11:59:57 +0900

    [17252] 삼삼한 수

--- Baekjoon/1주차/삼삼한수_김설희.py
+++ Baekjoon/1주차/삼삼한수_김설희.py
@@ -1,2 +1,16 @@
-print("testing git")
-print("testing git")
+n = int(input())
+ans = False
+
+if n == 0:
+    print("NO")
+    ans = True
+
+while n != 0:
+   if n % 3 == 2:
+        print("NO")
+        ans = True
+        break
+   n //= 3 # 나머지 1이어도 괜찮으니까
+
+if ans == False:
+    print("YES")
