98c7f23210d288ffc394b686645ae49adb88ebc7
unknown <tjrwls08088@naver.com>
2023-03-17 14:44:02 +0900

    [6131]완전제곱수

---
+++ Baekjoon/1주차/완전제곱수_양석진.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for(int a = 1; a<= 500; a++){
+            for(int b = 1; b<=500; b++){
+                if((Math.pow(a, 2) == Math.pow(b, 2) - N)){
+                    arrayList.add(a);
+                    arrayList.add(b);
+                    break;
+                }
+            }
+        }
+
+        bw.write(String.valueOf(arrayList.size()/2));
+        bw.close();
+    }
+}
