20b43ca8f18f27434e35450bdf5594b44e72d05b
sjkim <seoungjoo01@gmail.com>
2023-03-17 16:36:44 +0900

    [14697] 방 배정하기

---
+++ Baekjoon/1주차/방배정하기_김승주.py
@@ -0,0 +1,28 @@
+'''
+문제 : 방배정하기
+'''
+
+a, b, c , n = map(int, input().split())
+
+for i in range((n//a)+1):
+    for j in range((n//b)+1):
+        for k in range((n//c)+1):
+            if ( a*i + j*b + k*c )==n: 
+                print(1)
+                exit()
+print(0)
+
+
+''' 
+think about
+3중 for 문이 아닌 2중 for문으로 푼 결과는 부분 성공!
+
+왜?
+
+for i in range((n//a)+1):
+    for j in range((n//b)+1):
+        if (n- (a*i + b*j)) % c ==0:
+            print(1)
+            exit()
+print(0)
+'''
