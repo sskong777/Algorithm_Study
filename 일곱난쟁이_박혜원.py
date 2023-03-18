5c95dfbbec33915854c16a669978e9aeb02c2e8f
cherryJubilee <112672507+cherryJubilee@users.noreply.github.com>
2023-03-18 15:44:19 +0900

    [2851] 일곱난쟁이

---
+++ 일곱난쟁이_박혜원.py
@@ -0,0 +1,21 @@
+nanjeng_list = []
+for _ in range(9):
+    nanjeng_list.append(int(input()))
+
+
+def main():
+    for i, j in enumerate(nanjeng_list):
+        for k, l in enumerate(nanjeng_list):
+            if i != k:
+                total = sum(nanjeng_list)
+                total -= (j + l)
+                if total == 100:
+                    nanjeng_list.remove(j)
+                    nanjeng_list.remove(l)
+                    nanjeng_list.sort()
+                    return nanjeng_list
+
+
+answer_list = main()
+for i in answer_list:
+    print(i)
