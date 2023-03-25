a2de1f444f92c7eedf5eb0d1d6d1f57e56dc03a4
sjkim <seoungjoo01@gmail.com>
2023-03-25 11:02:40 +0900

    [Chore] 폴더구조, 파일명 변경 & 3주차 진행설명 추가

---
+++ Baekjoon/완전탐색/삼삼한수_김우원.java
@@ -0,0 +1,20 @@
+package com.kosa.aps;
+
+import java.util.Scanner;
+
+public class 삼삼한수_김우원 {//[17252] 삼삼한 수
+
+    public static void main(String[] args) {
+        int N = new Scanner(System.in).nextInt();
+        boolean flag = N != 0;
+        while (N > 0) {
+            if (N % 3 == 2) {
+                flag = false;
+                break;
+            }
+            N /= 3;
+        }
+        System.out.println(flag ? "YES" : "NO");
+    }
+
+}
