b0d3d3fe9737a4d528a45c9e1d561de74d525ee0
HyeonJeong <lhynjn9@gmail.com>
2023-03-30 15:58:22 +0900

    [1978] 소수찾기

---
+++ Baekjoon/정수론/소수찾기_이현정.py
@@ -0,0 +1,18 @@
+N = int(input())
+num = list(map(int, input().split()))
+ans = 0
+
+# 주어진 개수 만큼 소수 판별 진행
+for i in range(N):
+    if num[i] == 1:
+        pass
+    else:
+        check = 0 # 소수 판단을 위한 수
+        for j in range(2, num[i]):
+            if num[i] % j == 0:
+                check += 1
+                break
+        if check == 0:
+            ans += 1
+
+print(ans)
