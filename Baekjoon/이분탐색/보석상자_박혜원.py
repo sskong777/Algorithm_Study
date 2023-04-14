32fc9d487af83e0120f623932df4ffdbaf4262f5
cherryJubilee <hyewon5026@gmail.com>
2023-04-15 02:33:40 +0900

    [2792] 보석상자

--- Baekjoon/이분탐색/보석상자_박혜원.py
+++ Baekjoon/이분탐색/보석상자_박혜원.py
@@ -1,6 +1,6 @@
 # [2792] 보석상자
 # 나눠줄 보석의 수 (mid = 질투심), 질투심 최솟값 출력!
-# 인원수 n 색상 종류 수 m
+# 인원수 n 색상 종류 수 k
 
 
 n, k = map(int, input().split())
@@ -17,9 +17,10 @@ while start <= end:
             person_per_jewel += color // mid
         else:   # 나머지가 존재한다면? 해당 보석을 모두 나눠주어야 하니까 1명 더 추가
             person_per_jewel += (color // mid) + 1
-    if person_per_jewel <= n:    # 보석이 남았다면 = 나눠줘야 할 사람이 더 필요한 경우
-        answer = mid
+    if person_per_jewel <= n:    # mid가 보석의 최댓값인 7 이상이면 나눠줄 학생수의 최대가 5로 일정해진다.
+        answer = mid             # 답을 출력하고,  mid(질투심)의 최소를 찾기위해 end를 1씩 낮춰본다
         end = mid - 1
-    else:                 # 보석을 n명의 사람에게 전부 나눠줄 수 있다면
+    # 쥬얼리를 너무 조금씩 나눠서 받는 학생의 수가 많으므로, 나눠주는 보석의 수(mid) 높이기!
+    else:
         start = mid + 1
 print(answer)
