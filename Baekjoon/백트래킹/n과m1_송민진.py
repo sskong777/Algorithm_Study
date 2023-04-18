830d4c4b6908fa3d84d9286c101bc951791ffa75
Song Minjin <m_6595@naver.com>
2023-04-18 18:51:53 +0900

    [15649] n과 m 수정

--- Baekjoon/백트래킹/n과m1_송민진.py
+++ Baekjoon/백트래킹/n과m1_송민진.py
@@ -1,4 +1,4 @@
-# 풀이 1 - 임시
+# 풀이 1 - 조합
 from itertools import permutations
 
 n, m = map(int, input().split())
@@ -7,3 +7,22 @@ answer = list(permutations(arr, m))
 
 for ans in answer:
     print(*ans)
+
+# 풀이 2 - 백트랙킹
+n, m = map(int, input().split())
+arr = [i for i in range(1, n+1)]
+visited = [False] * (n+1)
+answer = []
+
+def solution(cnt, tmp):
+    if cnt == m:
+        print(*tmp)
+        return
+
+    for j in range(1, n+1):
+        if visited[j] == False:
+            visited[j] = True
+            solution(cnt + 1, tmp+[j])
+            visited[j] = False
+
+solution(0, [])
