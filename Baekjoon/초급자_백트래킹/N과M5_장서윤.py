459886a93e77bc184efc418c10d1837bdce2bc79
장서윤 <seoy316@naver.com>
2023-04-23 00:09:32 +0900

    [15654] N과M5

---
+++ Baekjoon/초급자_백트래킹/N과M5_장서윤.py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+card = list(map(int, input().split()))
+
+card.sort()
+
+isUsed = [False for _ in range(N + 1)]
+
+ans = list()
+
+def func(k):
+    if len(ans) == M:
+        print(' '.join(ans))
+        return
+
+    for i in range(0, len(card)):
+        if not isUsed[i]:
+            isUsed[i] = True
+            ans.append(str(card[i]))
+            func(k + 1)
+            isUsed[i] = False
+            ans.pop()
+
+func(0)
