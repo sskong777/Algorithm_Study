829d696802d25f1babf447bdd1874a799a1df33f
zzzjinwook <zzzjinwook@naver.com>
2023-04-25 23:19:23 +0900

    [1929] 소수구하기

---
+++ Baekjoon/초급편/소수구하기_홍진욱.py
@@ -0,0 +1,13 @@
+n =list(map(int,(input().split())))
+
+def prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+for i in range(n[0],n[1]+1):
+    if(prime(i)):
+        print(i)
