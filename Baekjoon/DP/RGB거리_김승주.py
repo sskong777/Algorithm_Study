d5e1707c09075fbed2ab48317bc580893731ef27
SeungJoo❤️ <seoungjoo01@gmail.com>
2023-04-28 18:23:19 +0900

    [1149] RGB 거리

---
+++ Baekjoon/DP/RGB거리_김승주.py
@@ -0,0 +1,14 @@
+
+n = int(input())
+
+colors =[]
+for i in range(n):
+    colors[i].append(list(map(int, input().split())))
+
+dp = [[0]*3 for _ in range(n)] 
+for i in range(1, n):
+    dp[i][0] = min(dp[i-1][1]+colors[i][0], dp[i-1][2]+colors[i][0])
+    dp[i][1] = min(dp[i-1][0]+colors[i][1], dp[i-1][2]+colors[i][1])
+    dp[i][2] = min(dp[i-1][0]+colors[i][2], dp[i-1][1]+colors[i][2])
+
+print(min(dp[n-1]))
