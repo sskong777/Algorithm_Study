67c709236ca7deab9602d8d5f1c09fb612cb9c56
SeungJoo❤️ <seoungjoo01@gmail.com>
2023-04-28 23:18:39 +0900

    [1149] RGB 거리

--- Baekjoon/DP/RGB거리_김승주.py
+++ Baekjoon/DP/RGB거리_김승주.py
@@ -1,11 +1,13 @@
 
 n = int(input())
 
-colors =[]
+colors = [] 
 for i in range(n):
-    colors[i].append(list(map(int, input().split())))
+    colors.append(list(map(int, input().split())))
 
 dp = [[0]*3 for _ in range(n)] 
+
+dp[0][0], dp[0][1] , dp[0][2]= colors[0][0], colors[0][1], colors[0][2]
 for i in range(1, n):
     dp[i][0] = min(dp[i-1][1]+colors[i][0], dp[i-1][2]+colors[i][0])
     dp[i][1] = min(dp[i-1][0]+colors[i][1], dp[i-1][2]+colors[i][1])
