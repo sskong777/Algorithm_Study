dce9090370b0d6cef9fd77e0be34a6b48c533677
tigre911 <94420939+tigre911@users.noreply.github.com>
2023-04-30 15:30:36 +0900

    [1929] 소수구하기

---
+++ Baekjoon/초급편/소수구하기_김진우.py
@@ -0,0 +1,23 @@
+'''n, m = map(int,input().split())
+arr = []
+
+for i in range(n, m+1) :
+    if i == 1 :
+        continue
+    for j in range(2, i+1):
+        if i%j == 0:
+            print(f'i, j : {i, j}')
+            break
+    else:
+        print(i)'''
+
+x, y = map(int, input().split())
+
+for i in range(x, y+1):
+    if i == 1:
+        continue
+    for j in range(2, int(i** 0.5)+1 ):
+        if i%j==0:
+            break
+    else:
+        print(i)
