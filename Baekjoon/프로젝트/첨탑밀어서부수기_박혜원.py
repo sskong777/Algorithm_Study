984126da2dcdc335c83cf94e480b32de305735d7
cherryJubilee <hyewon5026@gmail.com>
2023-06-07 00:28:51 +0900

    [28014] 첨탑 밀어서 부수기

--- Baekjoon/프로젝트/첨탑밀어서부수기_박혜원.py
+++ Baekjoon/프로젝트/첨탑밀어서부수기_박혜원.py
@@ -1,1 +1,10 @@
 # [28014] 첨탑 밀어서 부수기
+N = int(input())
+top_list = list(map(int, input().split()))  # [1 3 2 5 8 1]
+
+count = 1
+for i in range(1, N):
+    if top_list[i-1] <= top_list[i]:
+        count += 1
+
+print(count)
