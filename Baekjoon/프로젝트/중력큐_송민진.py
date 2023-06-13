81cacf81c8229a477e1a6aad48325479d877229e
송민진 <m_6595@naver.com>
2023-06-13 14:02:51 +0900

    [28078] 중력 큐

--- Baekjoon/프로젝트/중력큐_송민진.py
+++ Baekjoon/프로젝트/중력큐_송민진.py
@@ -2,44 +2,62 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-# 틀렸습니다 뜨는데,, 고쳐볼게여
-def do_action(queries):
+def solution(queries):
     global ball_cnt, wall_cnt
     if len(queries) == 1:
-        if queue:
-            checking = queue.popleft()
-            if checking == "b":
-                ball_cnt -= 1
-            elif checking == "w":
-                wall_cnt -= 1
+        pop()
     else:
         action, object = queries
         if action == "push":
-            queue.append(object)
-            if object == "b":
-                ball_cnt += 1
-            else:
-                wall_cnt += 1
+            push(object)
         if action == "count":
-            if object == "b":
-                print(ball_cnt)
-            elif object == "w":
-                print(wall_cnt)
-            return
+            count(object)
         if action == "rotate":
             rotate(object)
-        if angle == 90:
-            while queue:
-                if queue[0] == "w":
-                    break
-                queue.popleft()
-                ball_cnt -= 1
-        if angle == 270:
-            while queue:
-                if queue[-1] == "w":
-                    break
-                queue.pop()
-                ball_cnt -= 1
+    apply_gravity()
+
+
+def push(object):
+    global ball_cnt, wall_cnt
+    queue.append(object)
+    if object == "b":
+        ball_cnt += 1
+    else:
+        wall_cnt += 1
+
+
+def count(object):
+    if object == "b":
+        print(ball_cnt)
+    elif object == "w":
+        print(wall_cnt)
+
+
+def pop():
+    global ball_cnt, wall_cnt
+    if queue:
+        checking = queue.popleft()
+        if checking == "b":
+            ball_cnt -= 1
+        elif checking == "w":
+            wall_cnt -= 1
+
+
+def apply_gravity():
+    global ball_cnt
+    if angle == 90:
+        while queue:
+            if queue[0] == "w":
+                break
+            queue.popleft()
+            ball_cnt -= 1
+    if angle == 270:
+        while queue:
+            if queue[-1] == "w":
+                break
+            queue.pop()
+            ball_cnt -= 1
+
 
 def rotate(direction):
     global angle
@@ -60,5 +78,5 @@ wall_cnt = 0
 
 for _ in range(q):
     query = list(input().strip().split())
-    do_action(query)
+    solution(query)
 
