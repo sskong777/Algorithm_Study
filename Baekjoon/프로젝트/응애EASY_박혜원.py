533158bb3823d17d1a7a0288ea55ce655aec23ff
cherryJubilee <hyewon5026@gmail.com>
2023-06-13 13:29:07 +0000

    [28088]응애EASY

--- Baekjoon/프로젝트/응애EASY_박혜원.py
+++ Baekjoon/프로젝트/응애EASY_박혜원.py
@@ -1,1 +1,22 @@
 #[28088] 응애(EASY)
+"""
+동아리원의 수 N, 
+처음에 인사를 하는 사람들의 수 M, 
+인사를 진행하는 횟수 K, 
+M명의 동아리원이 인사를 시작하는 위치
+"""
+from collections import deque
+
+N, M, K = map(int, input().split())
+hi = [0] * N+1
+
+queue = deque(int(input()) for _ in range(M))
+
+for _ in range(K):
+# temp_queue : 다음 회차에서 인사를 시작할 동아리원들의 목록을 따로 관리
+    temp_queue = deque()
+
+    while queue:
+        idx = queue.popleft()
+
+        if 
