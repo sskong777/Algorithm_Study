490949a78cdd8a30654bae80094dcb10ccb4dca7
Daniel Choi <schoiak@connect.ust.hk>
2023-06-16 11:44:21 +0900

    스파이

--- Baekjoon/프로젝트/스파이_최승열.py
+++ Baekjoon/프로젝트/스파이_최승열.py
@@ -2,6 +2,7 @@ f=lambda:map(int,input().split())
 N,M=f()
 P=list(f())+list(f())
 def dfs(n,m,l):
-  if n==0:return 0 if m>0 else 1
+  if m<=0:return 6**n
+  elif n==0:return 0
   return sum(dfs(n-1,m-P[p]+(P[p]*1/2*(l in[p,(p+3)%6])),p)for p in range(6))
 print(dfs(N,M,-1))
