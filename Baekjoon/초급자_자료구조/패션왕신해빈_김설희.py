a04825d776f37169f056d461aa3795ca69a45d2a
seolhi <seolhi1212@gmail.com>
2023-06-20 09:08:30 +0900

    [9375] 패션왕 신해빈

---
+++ Baekjoon/초급자_자료구조/패션왕신해빈_김설희.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+for _ in range(n):
+    clothes = {}
+    k = int(input())
+
+    for _ in range(k):
+
+        name, kind = input().split()
+
+        if kind not in clothes:
+            clothes[kind] = []
+
+        clothes[kind].append(name)
+
+    count = 1
+    for key in clothes:
+        count *= len(clothes[key]) + 1
+
+    print(count - 1)
