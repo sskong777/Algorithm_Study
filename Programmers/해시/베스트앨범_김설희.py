1e7c05d2e558815f494a3260e57d5ccf51b96e4b
seolhi <seolhi1212@gmail.com>
2023-07-11 16:59:10 +0900

    [해시] 레벨1

---
+++ Programmers/해시/베스트앨범_김설희.py
@@ -0,0 +1,33 @@
+def solution(genres, plays):
+    musics = {}
+    total_genres = {}
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        if g not in musics:
+            musics[g] = [(i, p)]
+        else:
+            musics[g].append((i, p))
+
+        if g not in total_genres:
+            total_genres[g] = p
+        else:
+            total_genres[g] += p
+
+    total_genres = sorted(total_genres.items(), key=lambda x: -x[1])
+
+    for g in total_genres:
+        genre_name = g[0]
+        musics[genre_name] = sorted(musics[genre_name], key=lambda x: (-x[1], x[0]))
+
+    print(musics)
+    print(total_genres)
+
+    result = []
+    for g in total_genres:
+        cnt = 0
+        for i, p in musics[g[0]]:
+            cnt += 1
+            result.append(i)
+            if cnt >= 2:
+                break
+
+    return result
