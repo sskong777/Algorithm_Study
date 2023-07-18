9cc3e44d560816ae189be8dcb86b017af3cedb8d
seolhi <seolhi1212@gmail.com>
2023-07-18 21:50:47 +0900

    [인프런] 파이썬 알고리즘

--- inflearn/섹션6. 완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초/11. 수들의 조합(DFS).py
+++ inflearn/섹션6. 완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초/11. 수들의 조합(DFS).py
@@ -7,6 +7,9 @@ input
 output
 2
 '''
+
+# 내가 푼 정답 풀이
+'''
 import sys
 
 def dfs(L, total, check):
@@ -19,6 +22,29 @@ def dfs(L, total, check):
         dfs(L + 1, total + nums[L], check + 1)
         dfs(L + 1, total, check)
 
+
+input = sys.stdin.readline
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+m = int(input())
+cnt = 0
+
+dfs(0, 0, 0)
+
+print(cnt)
+'''
+
+# 다른 풀이 (강의 풀이) - 받는 건 똑같은데 구현을 다르게 함
+import sys
+
+def dfs(L, s, total):
+    global cnt
+    if L == k:
+        if total % m == 0:
+            cnt += 1
+    else:
+        for i in range(s, n):
+            dfs(L+1, i+1, total + nums[i])
 
 input = sys.stdin.readline
 n, k = map(int, input().split())
